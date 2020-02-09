3e5c0fd5f4b84353a1953c6381b46fb54224eb8e
ABC FOR JAVA TESTING <ABC FOR JAVA TESTING@DESKTOP-CCRN0FB>
2020-02-09 09:39:36 +0000

    commit one

---
+++ PageObjectModel/src/com/abc/MagentoLogin/MagentoLogin.java
@@ -0,0 +1,32 @@
+package com.abc.MagentoLogin;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.abc.MagentoObjects.Login;
+import com.abc.MagentoObjects.Main;
+import com.abc.MagentoObjects.Welcome;
+
+public class MagentoLogin {
+
+	public static void main(String[] args) {
+		String url ="https://www.magento.com";
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get(url);
+		Welcome w=new Welcome(driver);
+		w.clickOnMyaccount();
+		Login l = new Login(driver);
+		l.enterEmail();
+		
+		l.enterpassword();
+		l.clickOnLogin();
+		Main m = new Main(driver);
+		m.clickOnLogout();
+		driver.close();
+	}
+
+}
